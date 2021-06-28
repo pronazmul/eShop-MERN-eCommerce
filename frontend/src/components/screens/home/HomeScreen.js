10cd643ac48e7c02d26a822285b4fdb304702ee7
pronazmul <developernazmul@gmail.com>
2021-06-29 01:20:28 +0600

    Axios Request using Actions, Dispatch, Load Data on Screen

--- frontend/src/components/screens/home/HomeScreen.js
+++ frontend/src/components/screens/home/HomeScreen.js
@@ -1,29 +1,40 @@
-import React, { useEffect, useState } from 'react'
-import { Col, Row } from 'react-bootstrap'
+import React, { useEffect } from 'react'
+import { Alert, Col, Row, Spinner } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import { listProducts } from '../../../actions/productActions'
 import Product from './Product'
-import axios from 'axios'
 
 const HomeScreen = () => {
-  const [Products, setProducts] = useState([])
+  const dispatch = useDispatch()
+
   useEffect(() => {
-    const fetchProduct = async () => {
-      const { data } = await axios.get('/api/products')
-      setProducts(data)
-    }
-    fetchProduct()
-  }, [])
+    dispatch(listProducts())
+  }, [dispatch])
+
+  const productList = useSelector((state) => state.productList)
+  const { loading, error, products } = productList
 
   return (
     <>
-      <Row>
+      <Row className=''>
         <h1 className='text-align-center'>Latest Products</h1>
       </Row>
       <Row>
-        {Products.map((item) => (
-          <Col key={item._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={item} />
-          </Col>
-        ))}
+        {loading ? (
+          <div className='d-flex justify-content-center mt-5'>
+            <Spinner animation='grow' />
+          </div>
+        ) : error ? (
+          <Alert variant='danger'>
+            <Alert.Heading>{error}</Alert.Heading>
+          </Alert>
+        ) : (
+          products.map((item) => (
+            <Col key={item._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={item} />
+            </Col>
+          ))
+        )}
       </Row>
     </>
   )
